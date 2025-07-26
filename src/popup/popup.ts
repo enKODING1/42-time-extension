c892d6aa7be8a15daec8bb6ec30a23670d002d0f
Seongjin Kang <65354945+enKODING1@users.noreply.github.com>
2025-07-26 14:41:27 +0900

    feat: popup.html ui 수정

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const btn = document.getElementById('intra-link');
-  btn?.addEventListener('click', () => {
+  // 42 Intra 링크
+  const intraBtn = document.getElementById('intra-link');
+  intraBtn?.addEventListener('click', () => {
     chrome.tabs.create({ url: 'https://intra.42.fr' });
+  });
+
+  // GitHub 링크
+  const githubBtn = document.getElementById('github-link');
+  githubBtn?.addEventListener('click', () => {
+    chrome.tabs.create({ url: 'https://github.com/enkoding1/42-time-extension' });
   });
 }); 
